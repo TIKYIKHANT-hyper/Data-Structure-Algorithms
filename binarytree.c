93178232a7927cb770308c3d09972519eea58ef8
TIKYIKHANT-hyper <tikyikhant2334@gmail.com>
2023-11-12 02:50:29 +0300

    updated binarytree

--- binarytree.c
+++ binarytree.c
@@ -1,27 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-struct node{
+struct treeNode{
+    struct treeNode *leftPtr;
     int data;
-    struct node * left;
-    struct node * right;
+    struct treeNode *rightPtr;
 };
 
-struct node * createnode(int data){
-    struct node * newNode = (struct node *)malloc(sizeof(struct node));
-    newNode->left = NULL;
-    newNode->right = NULL;
-    newNode->data = data;
-    return newNode;
+typedef struct treeNode TreeNode;
+typedef TreeNode *TreeNodePtr;
+
+void insertNode(TreeNodePtr *treePtr, int value);
+void inOrder(TreeNodePtr treePtr);
+int main(){
+    TreeNodePtr rootPtr = NULL;
+    puts("numbers generated:");
+    for(int i = 1; i<=10; ++i){
+        int item = rand() % 15;
+        printf("%3i",item);
+        insertNode(&rootPtr, item);
+    }
+    puts("inOrder traversal:");
+    inOrder(rootPtr);
+    return 0;
 }
 
-int main(){
-    struct node * root = createnode(1);
-    root->left = createnode(2);
-    root->right = createnode(3);
-    root->left->left = createnode(4);
-    root->left->right = createnode(5);
-    root->right->left = createnode(6);
-    root->right->right = createnode(7);
-    return 0;
+void insertNode(TreeNodePtr *treePtr, int value){
+    if(*treePtr == NULL){
+        *treePtr = malloc(sizeof(TreeNode));
+        if(*treePtr != NULL){
+            (*treePtr)->data = value;
+            (*treePtr)->leftPtr = NULL;
+            (*treePtr)->rightPtr = NULL;
+        }
+        else{
+            printf("%i not inserted. No memory available.\n",value);
+        }
+    }
+    else{
+        if(value < (*treePtr)->data){
+            insertNode((&(*treePtr)->leftPtr),value);
+        }
+        else if(value > (*treePtr)->data){
+            insertNode((&(*treePtr)->rightPtr),value);
+        }
+        else{
+            printf("%s","dup");
+        }
+    }
+}
+
+void inOrder(TreeNodePtr treePtr){
+    if(treePtr!=NULL){
+        inOrder(treePtr->leftPtr);
+        printf(" %3i ",treePtr->data);
+        inOrder(treePtr->rightPtr);
+    }
 }
