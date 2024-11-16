12bb217c9e4c6b2ac351771d0159b93e6c9e181c
Ti Kyi Khant <tikyikhant2334@gmail.com>
2024-11-17 02:21:00 +0300

    bubblesort updated

--- bubblesort.c
+++ bubblesort.c
@@ -1,19 +1,45 @@
-//
-// Created by TIKYI on 3/14/2024.
-//
 #include <stdio.h>
-void swap(int *num1ptr, int *num2ptr);
+#include <stdlib.h>
+#include <time.h>
+#define displayName(var) #var
+#define SIZE 14
 
-int main(){
-    int testone = 10;
-    int testtwo = 20;
-    printf("Before:test one is %i and test two is %i\n",testone,testtwo);
-    swap(&testone,&testtwo);
-    printf("After:test one is %i and test two is %i\n",testone,testtwo);
+void arrDisplay(int arr[], int size);
+void bubbleSort(int arr[], int size);
+int main() {
+    srand(time(NULL));
+    int arrayOne[SIZE];
+    for(int i = 0; i < SIZE; i++) {
+        arrayOne[i] = rand() % 100;
+    }
+    arrDisplay(arrayOne, SIZE);
+    bubbleSort(arrayOne, SIZE);
+    arrDisplay(arrayOne, SIZE);
+    return 0;
 }
 
-void swap(int *num1ptr, int *num2ptr){
-    int hold = *num1ptr;
-    *num1ptr = *num2ptr;
-    *num2ptr = hold;
+void arrDisplay(int arr[], int size) {
+    int counter = 0;
+    printf("%s = ",displayName(int arr[]));
+    printf("[");
+    while(counter < size) {
+        printf("%d", arr[counter]);
+        if(counter < size - 1) {
+            printf(",");
+        }
+        counter++;
+    }
+    printf("]\n");
+}
+
+void bubbleSort(int arr[], int size) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1 ; j++) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
---
+++ cmake-build-debug/CMakeFiles/Data_Structure_Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Data_Structure_Algorithms.dir/bubblesort.c.obj"
+  "CMakeFiles/Data_Structure_Algorithms.dir/bubblesort.c.obj.d"
+  "Data_Structure_Algorithms.exe"
+  "Data_Structure_Algorithms.exe.manifest"
+  "Data_Structure_Algorithms.pdb"
+  "libData_Structure_Algorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Data_Structure_Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
