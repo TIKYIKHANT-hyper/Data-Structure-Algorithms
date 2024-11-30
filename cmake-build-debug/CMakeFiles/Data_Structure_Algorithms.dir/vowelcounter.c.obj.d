cd6a1a2d84f0392cfe571f80170aef0f033c423e
Ti Kyi Khant <tikyikhant2334@gmail.com>
2024-11-30 21:38:17 +0300

    vowelcounter updated

--- cmake-build-debug/CMakeFiles/Data_Structure_Algorithms.dir/vowelcounter.c.obj.d
+++ cmake-build-debug/CMakeFiles/Data_Structure_Algorithms.dir/vowelcounter.c.obj.d
@@ -10,14 +10,4 @@ CMakeFiles/Data_Structure_Algorithms.dir/vowelcounter.c.obj: \
  C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
  C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
  C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/stdlib.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/limits.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/syslimits.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/limits.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/malloc.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mm_malloc.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/errno.h
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.3/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
--- vowelcounter.c
+++ vowelcounter.c
@@ -1,89 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int isVowel(char c);
-int vowelCheck(char *str);
-
+int vowelCheck(char *str, char *upstr);
+int getLenArrStr(char *str);
 
 int main() {
-    char x[60] = "Apple,Orange,Lemon,pineapple.";
-    int res = vowelCheck(x);
-    printf("%d\n", res);
+    char x[60] = "Apple,Orange,Lemon,pineapple,avocado.";
+    char test[100];
+    int res = vowelCheck(x, test);
+    printf("Result: %s\n", test);
+    printf("Words Count: %d\n", res);
     return 0;
 }
 
 int getLenArrStr(char *str) {
     unsigned int count = 0;
-    while(*str != '\0') {
+    while (*str != '\0') {
         count++;
         str++;
     }
     return count;
 }
 
-int arrInitStr(char *str) {
-    int len = getLenArrStr(str);
-
-}
 int isVowel(char c) {
-    if( (c >= 65 && c <= 90) || (c >= 97 && c <= 122) ) {
-        if(c >= 65 && c <= 90) {
-            c = c + 32;
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        if (c >= 'A' && c <= 'Z') {
+            c = c + 32; // Convert uppercase to lowercase
         }
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-            return 1;
-        } else {
-            return 0;
-        }
-    }else {
-        return -1;
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     }
+    return 0;
 }
 
-int vowelCheck(char *str) {
-    int first = 0;
-    int last = 0;
+int vowelCheck(char *str, char *upstr) {
+    int first = 0, last = 0;
     int wordCount = 0;
-    int firstCheck = 1;
-    int charCount = 0;
-    int charCurWord = 0;
-    while(*str != '.') {
-        if(isVowel(*str) && firstCheck == 1) {
-            first = 1;
-            firstCheck = 0;
+    char *start = NULL;
+
+    while (*str != '\0') {
+        if (*str != ',' && *str != ' ' && start == NULL) {
+            start = str;
+            first = isVowel(*str);
         }
-        if(*str == ',') {
-            str--;
-            if(isVowel(*str)){
-                last = 1;
-                str++;
-            }
-            else {
+
+        if (*str == ',' || *str == '.') {
+            if (start != NULL) {
+                char *end = str - 1;
+                last = isVowel(*end);
+
+                if (first == 1 && last == 1) {
+                    while (start <= end) {
+                        *upstr++ = *start++;
+                    }
+                    *upstr++ = ',';
+                    wordCount++;
+                }
+                start = NULL;
+                first = 0;
                 last = 0;
-                str++;
             }
-        if(first == 1 && last == 1) {
-            wordCount++;
-            first = 0;
-            last = 0;
-            firstCheck = 1;
-            charCurWord = 0;
         }
-        else {
-            first = 0;
-            last = 0;
-            firstCheck = 1;
-            charCount = charCount - charCurWord;
-            charCurWord = 0;
-        }
-            str++;
-        }
-        else
-        {
-            str++;
-            charCurWord++;
-            charCount++;
-        }
+        str++;
     }
+
+    if (upstr != NULL && *(upstr - 1) == ',') {
+        *(upstr - 1) = '\0';
+    } else {
+        *upstr = '\0';
+    }
+
     return wordCount;
 }
